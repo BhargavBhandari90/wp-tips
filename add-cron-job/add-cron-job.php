5317d7b016b86dd386d10c74f129e461d03db8ce
bhargav <bhargav@buddyboss.com>
2022-01-03 17:28:51 +0530

    Add code doc

--- add-cron-job/add-cron-job.php
+++ add-cron-job/add-cron-job.php
@@ -4,6 +4,15 @@
  * After adding, YOU HAVE TO RE-ACTIVATE THE PLUGIN.
  */
 
+
+/**
+ * Add new time interval for cron job.
+ *
+ * Here we are adding 1 minite cron job.
+ *
+ * @param array $schedules
+ * @return void
+ */
 function custom_every_minute_schedule( $schedules ) {
 
 	// add a 'everyminute' schedule to the existing set
@@ -16,6 +25,11 @@ function custom_every_minute_schedule( $schedules ) {
 
 add_filter( 'cron_schedules', 'custom_every_minute_schedule' );
 
+/**
+ * Scedule cron job.
+ *
+ * @return void
+ */
 function custom_core_activate() {
 	if ( ! wp_next_scheduled( 'custom_every_minute_event' ) ) {
 		wp_schedule_event( time(), 'everyminute', 'custom_every_minute_event' );
@@ -26,6 +40,9 @@ register_activation_hook( __FILE__, 'custom_core_activate' );
 
 add_action( 'custom_every_minute_event', 'custom_every_minute_cronjob' );
 
+/**
+ * Do whatever you want to do in the cron job.
+ */
 function custom_every_minute_cronjob() {
 	error_log( date( 'Y-m-d H:i:s', time() ) );
 	add_option( 'custom_crone_run_at', date( 'Y-m-d H:i:s', time() ) );
